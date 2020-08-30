6cef1a8688490eea7bc2c4c477987ce5455bc298
Kraftbar <Gautenybo@gmail.com>
2020-08-30 15:04:50 +0200

    multiline

--- docs/mySetup/dots/_4myFunc.el
+++ docs/mySetup/dots/_4myFunc.el
@@ -1,6 +1,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Promt user before ending session (want this at shutdown, not impl yet) ;;
+;;               useful cnlang functions                                  ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -8,9 +8,49 @@
  
 
 (defun transelate-to-chinese (start end )
-  (interactive "r")
-  (shell-command  (concat "~/.emacs.d/__transelate-to-chinese.sh " (buffer-substring-no-properties start end) ))    
-  (insert-buffer "*Shell Command Output*")
+    "missing:
+        - multiline region
+        - no highlight
+"
+    (interactive "r")
+
+    (goto-char end)
+    (forward-line 1)
+    (while (>= (point) start)
+      (shell-command  (concat
+                        "~/.emacs.d/__transelate-to-chinese.sh \""
+                        (buffer-substring-no-properties       (line-beginning-position)       (line-end-position)     )
+                        "\""      ))
+      (kill-line 1)
+      (insert-buffer "*Shell Command Output*")
+      (forward-line -1)
+      )
+
 )
+
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                 snippets                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+;; (cl-case  system-type
+;;           (cywing  "Running cywing")
+;;           (darwing "Running Mac Osx")
+;;           (gnu/linux "Running Linux")
+;; )
+
+
+
+;; ELISP> (shell-command-to-string "pwd")
+;; "/home/tux/PycharmProjects/ocaml/prelude\n"
 
 
